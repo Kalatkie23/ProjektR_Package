78bb77bd03ec09b8f44bd0116c9e1bffdb8a19db
Kalatkie23 <37913679+Kalatkie23@users.noreply.github.com>
2022-01-09 16:51:41 +0100

    Delete plot.TempConvert.R

--- R/plot.TempConvert.R
+++
@@ -1,8 +0,0 @@
-plot.TempConvert <-
-function(x,...){
-    data<-data.frame(
-    name=c(x$Temperatura,x$Konwersja),
-    value=c(x$Temp,x$Konw)
-  )
-  barplot(height=data$value, names=data$name, col=rgb(0.2,0.4) )
-}
